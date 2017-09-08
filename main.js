609aa59bc7ed92759a75e1e47502446419642a61
Pontus Persson <pontus.alexander@gmail.com>
2017-09-08 19:36:41 +0200

    Define square.

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ rect.size.set(200, 60);
 rect.position.set(20, 10);
 shapes.push(rect);
 
+const square = new Shape();
+square.color.copy(Colors.PINK);
+square.size.set(120, 120);
+square.position.set(200, 100);
+shapes.push(square);
+
 shapes.forEach(shape => {
     shape.draw(context);
 });
