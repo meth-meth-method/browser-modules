96319a8a5bd83bb27e26b04f2da56c495b08249a
Pontus Persson <pontus.alexander@gmail.com>
2017-09-08 19:36:30 +0200

    Draw rect.

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+const shapes = [];
+
 const rect = new Shape();
 rect.color.copy(Colors.PURPLE);
 rect.size.set(200, 60);
 rect.position.set(20, 10);
+shapes.push(rect);
+
+shapes.forEach(shape => {
+    shape.draw(context);
+});
